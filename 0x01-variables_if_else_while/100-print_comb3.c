00fabd6d00dae922a502d224b573a8d97b2d3de5
TobiMoscow <tobimoscow@gmail.com>
2022-09-09 09:11:45 -0700

    PrintCmb

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -9,29 +9,29 @@
 
 int main(void)
 {
-		int digit1 = 0, digit2;
+	int digit1 = 0, digit2;
 	
-		while (digit1 <= 9)
+	while (digit1 <= 9)
+	{
+		digit2= 0;
+		whille(digit2 <= 9)
 		{
-			 digit2= 0;
-				whille(digit2 <= 9)
+			if (digit1 != digit2 && digit1 < digit2)
+			{
+				putchar(digit1 + 48);
+				putchar(digit2 + 48);
+
+				if (digit1 + digit2 != 17)
 				{
-					if (digit1 != digit2 && digit1 < digit2)
-					{
-						putchar(digit1 + 48);
-						putchar(digit2 + 48);
-
-						if (digit1 + digit2 != 17)
-						{
-							putchar(',');
-							putchar(' ');
-						}
-					}
-					++digit2;
+					putchar(',');
+					putchar(' ');
 				}
-				++digit1;
+			}
+			++digit2;
 		}
-		putchar('\n');
+		++digit1;
+	}
+	putchar('\n');
 
-		return (0);
+	return (0);
 }
