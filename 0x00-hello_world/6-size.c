63d11c58652bca2e02636c479b9fde9e714517f4
TobiMoscow <tobimoscow@gmail.com>
2022-09-08 21:52:11 -0700

    Size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -9,12 +9,11 @@
 int main(void)
 {
 	/* sizeof evaluates the size of a variable */
-		printf("Size of a char: %lubyte(s)\n", sizeof(char));
-			printf("Size of an int: %lubyte(s)\n", sizeof(int));
-				printf("Size of an double: lubyte(s)\n", sizeof(double));
-					printf("Size of a long int: %lubyte(s)\n", sizeof(long int));
-						printf("Size of a long long int: %lu byte(s)\n", sizeof(long long int));
-							printf("Size of a float: %lu byte(s)\n", sizeof(float));
-								return (0);
+	printf("Size of a char: %i byte(s)\n", sizeof(char));
+	printf("Size of an int: %i byte(s)\n", sizeof(int));
+        printf("Size of a long int: %i byte(s)\n", sizeof(long int));
+	printf("Size of a long long int: %i byte(s)\n", sizeof(long long int));
+        printf("Size of a float: %i byte(s)\n", sizeof(float));
+	return (0);
 
 }
