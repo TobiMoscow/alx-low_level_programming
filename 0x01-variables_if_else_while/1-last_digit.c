5c907834fa7b28662852276e3f3ce26240b483b9
TobiMoscow <tobimoscow@gmail.com>
2022-09-09 05:49:56 -0700

    Last Digit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -7,15 +7,14 @@
  * Description: print the value of n status:
  * greater than, is zero and is not less than 6.     
  * Return: Always O (Success)
- */
-
+*/
 int main(void)
 {
 		int n, digit;
 		srand(time(0));
 		n = rand() - RAND_MAX / 2;
-		/*my code*/
-		digit = n % 10; /*gets last digit*/
+		/* my code* /
+		digit = n % 10; / *gets last digit */
 		if (digit > 5)
 			printf("Last digit of %i is %i and is greater than 5\n", n, digit);
 		else if (digit == 0)
