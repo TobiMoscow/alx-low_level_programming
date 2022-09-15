2a263ce9ecc9c407b07e3ef29d7af21a2403d763
TobiMoscow <tobimoscow@gmail.com>
2022-09-15 05:12:00 -0700

    Fixes

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -11,17 +11,17 @@ void more_numbers(void)
 	int num, row, count;
 
 	for (row = 1; row <= 10; ++row)
+	{
+		for (count = 0; count <= 14; ++count)
 		{
-			for (count = 0; count <= 14; ++count)
+			num = count;
+			if (count > 9)
 			{
-				num = count;
-				if (count > 9)
-				{
-					_putchar(1 + 48);
-					num = count % 10;
-				}
-				_putchar(num + 48);
+				_putchar(1 + 48);
+				num = count % 10;
 			}
-			_putchar('\n');
+			_putchar(num + 48);
 		}
+		_putchar('\n');
+	}
 }
