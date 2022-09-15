27a206d1962acd2890fcdab162eaf5d54af3e4ae
TobiMoscow <tobimoscow@gmail.com>
2022-09-15 06:03:12 -0700

    Update

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -4,7 +4,7 @@
  * print_square - print a square using the character #
  * @size: is the size of the square
  * Return: Always 0 (Success)
- * */
+*/
 
 void print_square(int size)
 {
@@ -15,10 +15,10 @@ void print_square(int size)
 		for (row = 1; row <= size; row++)
 		{
 			for (column = 1; column <= size; column++)
-				{
-					_putchar('#');
-				}
-				_putchar('\n');
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
 		}
 	}
 	else
