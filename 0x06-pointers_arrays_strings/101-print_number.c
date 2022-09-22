abc093eceb29a6cc856c08a8b6730c11d818e8a4
TobiMoscow <tobimoscow@gmail.com>
2022-09-22 04:48:22 -0700

    print

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,4 +1,4 @@
-#include"main.h"
+#include "main.h"
 
 /**
  * print_number - print an integer, without using long, arrays, or pointers
@@ -7,15 +7,15 @@
 
 void print_number(int n)
 {
-unsigned int num = n;
+	unsigned int num = n;
 
-if (n < 0)
-{
-_putchar('-');
-num = -num;
-}
-if ((num / 10) > 0)
-print_number(num / 10);
-_putchar((num % 10) + '0');
+	if (n < 0)
+	{
+		_putchar('-');
+		num = -num;
+	}
+	if ((num / 10) > 0)
+		print_number(num / 10);
+		_putchar((num % 10) + '0');
 
 }
