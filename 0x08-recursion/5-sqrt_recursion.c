9ba83cd0987723e0739e27e07ab583aaee4d5a69
TobiMoscow <tobimoscow@gmail.com>
2022-09-28 10:24:55 +0100

    root

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,41 +1,33 @@
+#include "holberton.h"
+
 /**
- * _sqrt_recursion_wrapper - a wrapper that does the recursion bit
- * @n : input number
- * @min: minimum number to guess
- * @max: maximum number to guess
- * Return: square root of @n or -1
+ * find_root - find square root of n, starting from the smallest possible, 0
+ * @n: n
+ * @root: test this root
+ * Return: natural square root, or -1 if not natural root
 */
-int _sqrt_recursion_wrapper(int n, int min, int max)
+
+int find_root(int n, int root)
 {
-	int guess, guess_squared;
-
-	guess = (min + max) / 2;		/* get guess (g) */
-	guess_squared = guess * guess;		/* square guess (sg) */
-
-	if (guess_squared == n)			/* if squared guess == n return guess */
-		return (guess);
-	else if (min == max)			/* this means n doesn't have a perfect square */
+	if (root * root > n)
 		return (-1);
-	else if (guess_squared < n)		/* overshoot g if sg is less than the n */
-		return (_sqrt_recursion_wrapper(n, guess + 1, max));
-	else					/* undershoot g if sg is more than the n */
-		return (_sqrt_recursion_wrapper(n, min, guess - 1));
+
+	if (root * root == n)
+		return (root);
+
+	return (find_root(n, root + 1));
 }
 
 /**
- * _sqrt_recursion - a function that returns the natural
- * square root of a number
- * @n: input number
- * Return: square root
+ * _sqrt_recursion - find natural square root of n
+ * @n: n
+ * Return: natural square root, or -1 if not natural root
 */
+
 int _sqrt_recursion(int n)
 {
-	if (n == 1)		/* sqrt(1) == 1 */
-		return (1);
-	else if (n == 0)	/* sqrt(0) == 0 */
-		return (0);
-	else if (n < 0)		/* sqrt(-n) == -1 (NA) */
+	if (n < 0)
 		return (-1);
-	else
-		return (_sqrt_recursion_wrapper(n, 1, n));
+
+	return (find_root(n, 0));
 }
