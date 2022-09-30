2d47b7af9a4452d97e85667d2d52ba9c0541258e
TobiMoscow <tobimoscow@gmail.com>
2022-09-30 13:13:57 -0700

    Name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - prints its name
+ * @argc: number of arguments
+ * @argv: arguments passed during execution
+ * Return: Always 0
+*/
+int main(int argc __attribute__((unused)), char *argv[])
+{
+	printf("%s\n", *argv);
+
+	return (0);
+}
